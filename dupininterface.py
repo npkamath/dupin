cb515fd949907f9f4f7966f2f7d34bffcc3c2d05
npkamath <npkamath@umich.edu>
2023-12-17 16:55:40 -0500

    dupininterface.py update

--- dupininterface.py
+++ dupininterface.py
@@ -7,7 +7,7 @@ import time
 from dupin.detect.costs import CostLinearFit
 from guppy import hpy
 
-h = hpy()
+
 
 
 random.seed(445)
@@ -25,12 +25,12 @@ def compute_python_cost_matrix(signal: np.ndarray, min_size: int = 3):
 def compute_cplus_cost_matrix(signal: np.ndarray):
     
     algo = _dupin.DupinAlgo()
-    algo.num_bkps = 5; 
+    algo.num_bkps = 4; 
     algo.num_timesteps = signal.shape[0]
     algo.num_parameters = signal.shape[1]
     algo.datum = np.asfortranarray(signal) 
     start1 = time.time()
-    algo.initialize_cost_matrix(algo.datum)
+    algo.initialize_cost_matrix()
     
     
 
@@ -45,41 +45,39 @@ def compute_cplus_cost_matrix(signal: np.ndarray):
 
 
 def generate_data():
-    soize = 200
-    data = (np.repeat([0, -200, -160, -201, -152, 200], soize) + np.random.random(soize*6)).reshape((-1, 3))
-    newdata = (np.repeat([0, 200, 0, 0], 180) + np.random.random(180*4)).reshape((-1,3))
+    soize = 1080
+    data = (np.repeat([0, -200, -160, -201, -152, 200], soize) + np.random.random(soize*6)).reshape((-1, 6))
+    newdata = (np.repeat([0, 200, 0, 0], 180) + np.random.random(180*4)).reshape((-1,6))
     data = np.concatenate((data, newdata), axis=0)
     return data
 
+def compute_cpp_operations(data):
+    cppcost_matrix, topcppbkps, cpptime = compute_cplus_cost_matrix(data)
+    return cppcost_matrix, topcppbkps, cpptime
 
+def compute_python_operations(data):
+    start_time = time.time()
+    dynp = rpt.Dynp(custom_cost=du.detect.CostLinearFit("l2"), jump=1)
+    sweepsweep = du.detect.SweepDetector(dynp, 10)
+    sweepsweep.fit(data)
+    end_time = time.time()
+    pythontime = end_time - start_time
+    return sweepsweep.change_points_, sweepsweep.opt_change_points_, pythontime
 
 def test_dupin():
     data = generate_data()
-    print(f"{data=}") 
-    
-    cppcost_matrix, topcppbkps, cpptime = compute_cplus_cost_matrix(data)
-    
-    
-    pycost_matrix = compute_python_cost_matrix(data)
-    print ("diff: ", pycost_matrix - cppcost_matrix)
-    
 
-    printdiff = np.allclose(cppcost_matrix, pycost_matrix, atol=1e-1, rtol= 1e-2)
+    # C++ Operations
+    cppcost_matrix, topcppbkps, cpptime = compute_cpp_operations(data)
 
-    print("Are the arrays the same?: ", printdiff) 
-    
-    start2 = time.time()
-    dynp = rpt.Dynp(custom_cost = du.detect.CostLinearFit("l2"), jump=1)
-    sweepsweep = du.detect.SweepDetector(dynp, 10)
-    sweepsweep.fit(data)
-    end2 = time.time()
-    pythontime= end2-start2
-    print("pythontime: ", pythontime)
-    multiplier = pythontime/cpptime
-    print("C++ is ", multiplier, " times faster!")
-    print(f"python breakpoints: {sweepsweep.change_points_} opt change points: {sweepsweep.opt_change_points_}")
+    # Python Operations
+    python_bkps, opt_python_bkps, pythontime = compute_python_operations(data)
+
+    # Comparing and printing results
+    multiplier = pythontime / cpptime
+    print("C++ is", multiplier, "times faster!")
+    print(f"Python breakpoints: {python_bkps} opt change points: {opt_python_bkps}")
     print("Top Down C++ breakpoints:", topcppbkps)
-    print (h.heap())
 
 if __name__ == "__main__":
     test_dupin()
